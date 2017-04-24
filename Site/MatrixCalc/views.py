fdfcdfd7386e70f4073cbdb718d9ac6f05c28f1b
Alexandr <ovdienkoalexandr@mail.ru>
2017-04-25 00:16:08 +0300

    asdfasdfasd

--- Site/MatrixCalc/views.py
+++ Site/MatrixCalc/views.py
@@ -2,6 +2,8 @@
 
 from django.http import JsonResponse
 from django.shortcuts import render
+from setuptools.compat import unicode
+
 from MatrixCalc.Matrix import *
 
 
@@ -22,17 +24,17 @@ def index(request):
                 sm[i].append(int(request.POST.get('SecondMatrix:' + str(i) + '_' + str(j), 0)))
         answer = 0
         # использование методов матричного модуля
-        if operation == u"Сложение матриц":
+        if unicode(operation) == u"Сложение матриц":
             answer = MatrixsSum(fm, sm)
-        elif operation == u"Вычитание матриц":
+        elif unicode(operation)== u"Вычитание матриц":
             answer = MatrixsSub(fm, sm)
-        elif operation == u"Умножение матриц":
+        elif unicode(operation) == u"Умножение матриц":
             answer = MatrixsMult(fm, sm)
-        elif operation == u"Умножение матрицы на число":
+        elif unicode(operation) == u"Умножение матрицы на число":
             answer = MatrixMultNumber(fm, number)
-        elif operation == u"Найти определитель":
+        elif unicode(operation) == u"Найти определитель":
             answer = MatrixDeterminant(fm)
-        elif operation == u"Транспонирование":
+        elif unicode(operation) == u"Транспонирование":
             answer = TransposeMatrix(fm)
         # возвращение результата в формате json
         return JsonResponse({'answer': answer})
